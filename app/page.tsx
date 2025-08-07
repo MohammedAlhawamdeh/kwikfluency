fcfaf2cafc4e1d60cda050ca0f90dca8d9e60e26
mohammedAlhawamdeh <hawamdeh9393@gmail.com>
2025-08-07 20:43:17 +0300

    Refactor layout and global styles to use Open Sans font; update Home component to restore original content structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1 className="font-black text-9xl">Kwik Fluency</h1>
+      <h4 className="font-sans font-light">This is sans</h4>
+      <p className="font-sans font-extralight">Hello from hell</p>
+    </>
+  );
 }
