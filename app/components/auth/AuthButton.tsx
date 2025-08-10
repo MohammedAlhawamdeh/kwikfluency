bd2bd7117897957ca5e4420c3b5a2a1da477bef8
mohammedAlhawamdeh <hawamdeh9393@gmail.com>
2025-08-10 16:57:23 +0300

    refactor: Remove obsolete authentication and password management code

--- app/components/auth/AuthButton.tsx
+++
@@ -1,8 +0,0 @@
-import { getUser } from '@/app/lib/actions/auth'
-import AuthButtonClient from './AuthButtonClient'
-
-export default async function AuthButton() {
-  const user = await getUser()
-  
-  return <AuthButtonClient user={user} />
-}
